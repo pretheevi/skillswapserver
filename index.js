815d0db00f56f26bb60e8dfd415101f26e4a23ce
pretheevi <pretheeviraj0805@gmail.com>
2025-12-08 22:02:40 +0530

    added client https to cors

--- index.js
+++ index.js
@@ -16,7 +16,12 @@ const Server = http.createServer(app);
 connectDB();
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://skillswap-aead1adi9-prethiveerajs-projects.vercel.app",
+    "https://skillswap-git-main-prethiveerajs-projects.vercel.app"
+  ]
+}));
 app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.method, req.url);
@@ -27,6 +32,7 @@ app.use("/api", authentication);
 app.use("/api", skillRoutes);
 app.use('/api', comment)
 
-Server.listen(process.env.PORT, "0.0.0.0", () =>
+const PORT = process.env.PORT || 8080;
+Server.listen(PORT, "0.0.0.0", () =>
   console.log(`listening to PORT:${process.env.PORT}...`)
 );
